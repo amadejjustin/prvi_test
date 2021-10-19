5e35ffcb2fcb785a23264148e2bc559d7945570a
amadejjustin <92815886+amadejjustin@users.noreply.github.com>
2021-10-19 19:31:41 +0200

    dodal gumb a+b

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        . # # # #
+        # # . # #
+        # # # # #
+        `)
+})
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
         basic.showString("f")
