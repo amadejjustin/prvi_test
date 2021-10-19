5a67596bc1b8b9b07d8eb18c578eb5060656f218
amadejjustin <92815886+amadejjustin@users.noreply.github.com>
2021-10-19 19:29:09 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showString("f")
+        basic.pause(500)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+})
